33e4cc1090742dc1679aabb7c608a5804f71c06c
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-02 05:00:35 -0700

    improving codebase.

--- macro-thetical.user.js
+++ macro-thetical.user.js
@@ -19,50 +19,72 @@ const maxFat = 133;
 const maxCarb = 20;
 const maxProtein = 110;
 
-function parseGrams(element) {
-    return parseFloat(element.text().replace(/\s+g/gi, ''));
-}
-
-function getRemainingFat() {
-    let fatElement = $('#dailyTotals > div > div:nth-child(3) > div > div.amount');
-    return maxFat - parseGrams(fatElement);
-}
-
-function getRemainingCarb() {
-    let carbElement = $('#dailyTotals > div.content.firstBlock > div:nth-child(5) > div > div.amount');
-    return maxCarb - parseGrams(carbElement);
-}
-
-function getRemainingProtein() {
-    let proteinElement = $('#dailyTotals > div.content.firstBlock > div:nth-child(7) > div > div.amount');
-    return maxProtein - parseGrams(proteinElement);
+function getRemainingMacros() {
+    function parseGrams(element) {
+        return parseFloat(element.text().replace(/\s+g/gi, ''));
+    }
+    function getRemainingFat() {
+        let fatElement = $('#dailyTotals > div > div:nth-child(3) > div > div.amount');
+        return maxFat - parseGrams(fatElement);
+    }
+    function getRemainingCarbs() {
+        let carbElement = $('#dailyTotals > div.content.firstBlock > div:nth-child(5) > div > div.amount');
+        return maxCarb - parseGrams(carbElement);
+    }
+    function getRemainingProtein() {
+        let proteinElement = $('#dailyTotals > div.content.firstBlock > div:nth-child(7) > div > div.amount');
+        return maxProtein - parseGrams(proteinElement);
+    }
+    return {
+        'fat': getRemainingFat(),
+        'carbs': getRemainingCarbs(),
+        'protein': getRemainingProtein()
+    };
 }
 
 function getInnerElement(elementId) {
-    let dailyTotals = $('#dailyTotals');
+    let customRowsElement = $('div#my-custom-rows');
     let selector = 'div#' + elementId;
     if ($(selector).length === 0) {
-        dailyTotals.append('<div id=' + elementId + ' class="content"></div>');
+        customRowsElement.append('<div id=' + elementId + ' class="content"></div>');
     }
     let resultElement = $(selector);
     return resultElement;
 }
 
+function initializeCustomRows() {
+    // Create all the rows
+    let customRowsSelector = 'div#my-custom-rows';
+    if ($(customRowsSelector).length === 0 || $(customRowsSelector).is(":hidden")) {
+        $('div#dailyTotals').append('<div id="my-custom-rows"></div>');
+    } else {
+        return;
+    }
+    // Gather Remainders
+    let remainingMacros = getRemainingMacros();
+    // Maxes
+    let myMaxes = getInnerElement('my-max');
+    myMaxes.text('Max Fat/Carb/Protein: ' + maxFat + ' / ' + maxCarb + ' / ' + maxProtein);
+    // Remainders
+    let myRemainders = getInnerElement('my-remainders');
+    myRemainders.text('Remaining Fat/Carb/Protein: ' + remainingMacros.fat + ' / ' + remainingMacros.carbs + ' / ' + remainingMacros.protein);
+    // EGGS!!!
+    let myEggs = getInnerElement('my-eggs');
+    myEggs.text('How many more eggs can I eat today? ' + Math.floor(Math.min(remainingMacros.fat / 5, remainingMacros.protein / 6)) + ' 🥚');
+    // Recreate everything when the page is updated
+    //$(customRowsSelector).on('remove', function() {
+    //    initializeCustomRows();
+    //});
+//    $('#foodlog > span').bind('destroyed', function() {
+//        $(customRowsSelector).remove();
+//        initializeCustomRows();
+//    });
+}
+
+
+
 (function() {
     'use strict';
-    setInterval(function(){
-        // Gather Remainders
-        let remainingFat = getRemainingFat();
-        let remainingCarb = getRemainingCarb();
-        let remainingProtein = getRemainingProtein();
-        // Maxes
-        let myMaxes = getInnerElement('my-max');
-        myMaxes.text('Max Fat/Carb/Protein: ' + maxFat + ' / ' + maxCarb + ' / ' + maxProtein);
-        // Remainders
-        let myRemainders = getInnerElement('my-remainders');
-        myRemainders.text('Remaining Fat/Carb/Protein: ' + remainingFat + ' / ' + remainingCarb + ' / ' + remainingProtein);
-        // EGGS!!!
-        let myEggs = getInnerElement('my-eggs');
-        myEggs.text('How many more eggs can I eat today? ' + Math.floor(Math.min(remainingFat / 5, remainingProtein / 6)) + ' 🥚');
-    }, 100);
+    setInterval(initializeCustomRows, 100);
+    //initializeCustomRows();
 })();
