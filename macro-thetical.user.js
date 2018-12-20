0991c23476b03f956f1c071f2b17eac37c74e8a0
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-20 15:34:47 -0700

    Refactoring for cleanliness

--- macro-thetical.user.js
+++ macro-thetical.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         macro-thetical
 // @namespace    http://paulbaker.io
-// @version      0.5.4
+// @version      0.5.5
 // @description  Reads my macros, prints out how many I have left, and some hypothetical foods I can still eat with my allowance :)
 // @author       Paul Nelson Baker
 // @match        https://www.fitbit.com/foods/log
@@ -16,9 +16,13 @@
 My macros are based on my body height/type/shape and my
 fitness goals. Get yours from your personal trainer :)
 */
-const maxFat = 133;
-const maxCarbs = 20;
-const maxProtein = 110;
+const maxValues = {
+    fat: 133,
+    carbs: 20,
+    protein: 110,
+}
+
+console.log('Using max macros', maxValues);
 
 function parseMacroValue(macroJQuerySelector) {
     let currentMacroElement = $(macroJQuerySelector);
@@ -27,59 +31,48 @@ function parseMacroValue(macroJQuerySelector) {
     return currentMacroValue;
 }
 
-function getRemainingMacros() {
+function getRemainingMacros(maxValues) {
     let fatSelector = '#dailyTotals > div > div:nth-child(3) > div > div.amount';
     let carbsSelector = '#dailyTotals > div.content.firstBlock > div:nth-child(5) > div > div.amount';
     let fiberSelector = '#dailyTotals > div.content.firstBlock > div:nth-child(4) > div > div.amount';
     let proteinSelector = '#dailyTotals > div.content.firstBlock > div:nth-child(7) > div > div.amount';
     return {
-        'fat': maxFat - parseMacroValue(fatSelector),
-        'carbs': maxCarbs - parseMacroValue(carbsSelector) + parseMacroValue(fiberSelector),
-        'protein': maxProtein - parseMacroValue(proteinSelector),
+        'fat': maxValues.fat - parseMacroValue(fatSelector),
+        'carbs': maxValues.carbs - parseMacroValue(carbsSelector) + parseMacroValue(fiberSelector),
+        'protein': maxValues.protein - parseMacroValue(proteinSelector),
     };
 }
 
-function getInnerElement(elementId) {
-    let customRowsElement = $('div#my-custom-rows');
-    let selector = 'div#' + elementId;
-    if ($(selector).length === 0) {
-        customRowsElement.append('<div id=' + elementId + ' class="content"></div>');
-    }
-    let resultElement = $(selector);
-    return resultElement;
-}
-
-function initializeCustomRows() {
+function createRowContainer() {
     // Create all the rows
     let customRowsSelector = 'div#my-custom-rows';
     if ($(customRowsSelector).length === 0 || $(customRowsSelector).is(":hidden")) {
         $('div#dailyTotals').append('<div id="my-custom-rows"></div>');
-    } else {
-        return;
+        console.log('Creating row container', $(customRowsSelector)[0]);
     }
-    // Gather Remainders
-    let remainingMacros = getRemainingMacros();
-    // Maxes
-    let myMaxes = getInnerElement('my-max');
-    myMaxes.text('Max Fat/Carb/Protein: ' + maxFat + ' / ' + maxCarbs + ' / ' + maxProtein);
-    // Remainders
-    let myRemainders = getInnerElement('my-remainders');
-    myRemainders.text('Remaining Fat/Carb/Protein: ' + remainingMacros.fat + ' / ' + remainingMacros.carbs + ' / ' + remainingMacros.protein);
-    // EGGS!!!
-    let myEggs = getInnerElement('my-eggs');
-    myEggs.text('How many more eggs can I eat today? ' + Math.floor(Math.min(remainingMacros.fat / 5, remainingMacros.protein / 6)) + ' 🥚');
-    // Recreate everything when the page is updated
-    //$(customRowsSelector).on('remove', function() {
-    //    initializeCustomRows();
-    //});
-//    $('#foodlog > span').bind('destroyed', function() {
-//        $(customRowsSelector).remove();
-//        initializeCustomRows();
-//    });
-    // setTimeout(initializeCustomRows, 100);
 }
 
+function createRow(rowElementId, rowText) {
+    createRowContainer();
 
+    let customRowsElement = $('div#my-custom-rows');
+    let selector = 'div#' + rowElementId;
+    if ($(selector).length === 0) {
+        customRowsElement.append('<div id=' + rowElementId + ' class="content"></div>');
+        let resultElement = $(selector);
+        resultElement.text(rowText);
+        console.log('Creating row inside container', resultElement[0]);
+    }
+}
+
+function initializeCustomRows() {
+    // Gather Remainders
+    let remainingMacros = getRemainingMacros(maxValues);
+    // Maxes (to remind me), Remainders, Remainders in terms of EGGS <3!!!
+    createRow('my-max', 'Max Fat/Net-Carb/Protein: ' + maxValues.fat + ' / ' + maxValues.carbs + ' / ' + maxValues.protein);
+    createRow('my-remainders', 'Remaining Fat/Net-Carb/Protein: ' + remainingMacros.fat + ' / ' + remainingMacros.carbs + ' / ' + remainingMacros.protein);
+    createRow('my-eggs', 'How many more eggs can I eat today? ' + Math.floor(Math.min(remainingMacros.fat / 5, remainingMacros.protein / 6)) + ' 🥚')
+}
 
 (function() {
     'use strict';
