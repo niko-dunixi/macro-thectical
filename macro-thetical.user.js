9e550de588031f42d70725c1058990a14c819a0f
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-20 15:35:03 -0700

    Updating version number

--- macro-thetical.user.js
+++ macro-thetical.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         macro-thetical
 // @namespace    http://paulbaker.io
-// @version      0.5.5
+// @version      0.5.6
 // @description  Reads my macros, prints out how many I have left, and some hypothetical foods I can still eat with my allowance :)
 // @author       Paul Nelson Baker
 // @match        https://www.fitbit.com/foods/log
